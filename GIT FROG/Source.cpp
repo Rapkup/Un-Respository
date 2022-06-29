f35887d855cda5ef0e9fdc7eda2bd36bb8dafd41
Rapkup <svinkapeppa75@gmail.com>
2022-06-29 14:15:42 +0300

    initial commit

---
+++ GIT FROG/Source.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <windows.h>
+#include <locale.h>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <stdlib.h>
+
+
+using namespace std;
+
+
+
+
+
+
+void main(void)
+{
+
+	system("pause");
+}
